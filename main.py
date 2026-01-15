e29512bc286e8a429f7a583615ae7b98134460c7
Oliver <obaker@users.noreply.github.com>
2026-01-15 21:08:29 +0000

    Delete non existent episodes, add file size and length

--- main.py
+++ main.py
@@ -1,34 +1,38 @@
 #!/usr/bin/env python3
 
+from datetime import timedelta
 from pathlib import Path
 
 from argparse import ArgumentParser
 from bs4 import BeautifulSoup
+from eyed3 import load as eyed3Load
+
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("rss")
     parser.add_argument("media")
-    parser.add_argument("server")
+    parser.add_argument("addr")
     args = parser.parse_args()
-    print(f"Server is {args.server}")
-    mediaStems = list()
-    mediaPaths = list()
+    mediaStems: list[str] = list()
+    mediaPaths: list[Path] = list()
     for item in Path(args.media).iterdir():
         mediaStems.append(item.stem)
         mediaPaths.append(item)
     with open(args.rss) as file:
         rssContents = file.read()
-    soup = BeautifulSoup(rssContents, "lxml-xml") # lxml installed from pip     
+    soup = BeautifulSoup(rssContents, "lxml-xml")  # lxml installed from pip
     rssItems = soup.find_all("item")
     for item in rssItems:
         if item.title.string in mediaStems:
             mediaId = mediaStems.index(item.title.string)
-            # print(item.enclosure.get("url"))
-            item.enclosure["url"] = args.server + mediaPaths[mediaId].as_posix()
-            # print(item.enclosure["url"])
+            item.enclosure["url"] = args.addr + mediaPaths[mediaId].as_posix()
+            audiofile = eyed3Load(mediaPaths[mediaId]).info
+            item.enclosure["length"] = audiofile.size_bytes
+            length: str = str(timedelta(seconds=round(audiofile.time_secs)))
+            item.find("itunes:duration").contents = length
         else:
-            print(f"Couldn't find {item.title.string}")
+            item.decompose()
     with open(args.rss, "w", encoding='utf-8') as file:
         file.write(str(soup))
 
